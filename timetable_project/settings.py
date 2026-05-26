a3b967728f03d54623d4a033fb63d19dc26c1d98
Om Kumar <omkumar00116@gmail.com>
2026-05-26 16:18:00 +0530

    configured render database

--- timetable_project/settings.py
+++ timetable_project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'timetable_app',
+    # 'timetable_app',
+    'timetable_app.apps.TimetableAppConfig',
     'django.contrib.humanize',
     'smart_selects',
 ]
